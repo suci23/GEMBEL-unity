5ec3bb3acf4bb562868df39379e8b0e299015e5b
Friza Skeith <riza@kamusnusantara.com>
2014-02-23 16:30:20 +0700

    Push pertama :)

---
+++ Assets/Script/GoToScene.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoToScene : MonoBehaviour {
+
+  
+	public string sceneName;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void OnMouseDown(){
+	}
+	void OnMouseUpAsButton(){
+		Application.LoadLevel(sceneName);
+	}
+    
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+GEMBEL-unity
+============
+lalalal~
