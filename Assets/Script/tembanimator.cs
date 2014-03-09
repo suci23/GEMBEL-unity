2cd6955941592f75498fca57fda638c3bcd10f0a
Friza Skeith <riza@kamusnusantara.com>
2014-03-09 13:24:21 +0700

    level delapan

---
+++ Assets/Script/tembanimator.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class tembanimator : MonoBehaviour {
+
+	public Sprite[] sprites;
+	public float framesPerSecond;
+	private SpriteRenderer spriteRenderer;
+	public string sceneName;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = renderer as SpriteRenderer;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnMouseUpAsButton(){
+		//print ("hai");
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		if (index < 3) {
+						index = index % sprites.Length;
+						spriteRenderer.sprite = sprites [index];
+				}
+		if(index == 3){
+			Application.LoadLevel(sceneName);
+		}
+	}
+}
